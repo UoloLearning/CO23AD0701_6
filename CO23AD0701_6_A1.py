0796e4589c3353c5e2c7aaa4ba5b98bfed8b3482
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-21 15:47:58 +0530

    Create CO23AD0701_6_A1.py

---
+++ CO23AD0701_6_A1.py
@@ -0,0 +1,7 @@
+age = float(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+if age>15 and height>5.5:
+    print("You get access to all rides!")
+else:
+    print("You get access to the kid's rides only!")
